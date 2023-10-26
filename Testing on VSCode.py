c6029c4e2bf0f65d59fa5a52cf9a147cf0494c87
John Venom <127551079+Big-Boss-666V@users.noreply.github.com>
2023-10-26 15:51:07 -0700

    Commenting and reorginzing

--- Testing on VSCode.py
+++ Testing on VSCode.py
@@ -1,27 +1,32 @@
 import vpython as vp
 
-x_min = -10
-x_max = 10
-length = x_max - x_min
-N = 16
-dx = length/N
-wire = []
+# region Constants 
 
-POI = vp.vec(0, 3, 0)
-current = 1e4      #positive implies current to the right, negative to the left
-mu_0 = 4*vp.pi*1e-7
-constant = mu_0*current/4/vp.pi
-
+POI = vp.vec(0, 3, 0)  # Our point of interest, Specific place we care about
+x_min = -10          # Left side of the wire
+x_max = 10           # Right side of the wire
+N = 16               # Number of loops
+current = 1e4        # positive implies current to the right, negative to the left
+mu_0 = 4*vp.pi*1e-7  # Constant in back of book of mu
 scale_factor = 1e4
+wire = []            # wire is made up of multiple clinders
+# endregion
+
+# region Intitial Calculation
+length = x_max - x_min     # The absolute lenght of wire
+dx = length/N              # loops per lenght 
+constant = mu_0*current/4/vp.pi   # This is from the eq B = mu*I/Area integral of ds cross r vec / r mag
+# endregion 
+
 
 B_tot = vp.vec(0,0,0)
 B_tot_arrow = vp.arrow( pos=POI, axis=B_tot*scale_factor, color=vp.vec(0.4, 0.4, 1))
 
-B_label = vp.label(pos=POI, text=1e3*B_tot.mag + "mT", xoffset=20, yoffset=-20, border=0, box=False, line=False)
-B_label.text = f"<i>B<sub>tot</sub></i> =  {1e3*B_tot.mag:.2f} mT"
+B_label = vp.label(pos=POI, text="mT", xoffset=20, yoffset=-20, border=0, box=False, line=False)
+#B_label.text = f"<i>B<sub>tot</sub></i> =  {1e3*B_tot.mag:.2f} mT"
 B_label.color = B_tot_arrow.color
 
-current_label = vp.label(pos=vp.vec(0.85*x_min, 0.15*x_max, 0), text="I = " + 1e-3*current + " kA", border=0, box=False, line=False)
+current_label = vp.label(pos=vp.vec(0.85*x_min, 0.15*x_max, 0), text="I", border=0, box=False, line=False)
 current_label.color = vp.vec(1, 0.1, 0.1)
 
 for x in vp.arange(x_min, x_max, dx):
@@ -48,10 +53,6 @@ for x in vp.arange(x_min, x_max, dx):
     B_tot += constant*vp.cross(ds, r)/vp.mag(r)**3
     B_tot_arrow.axis = B_tot*scale_factor
     B_label.text = f"<i>B<sub>tot</sub></i> =  {1e3*B_tot.mag:.2f} mT"
-
-
-
- 
 
 print("B_tot VECTOR at "+POI+" is "+B_tot)
 
