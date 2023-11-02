634fa5f44b20ad1b923e206a803e8c4db1d54c7d
John Venom <127551079+Big-Boss-666V@users.noreply.github.com>
2023-11-01 21:44:56 -0700

    Added axis with methods

--- Main Code.py
+++ Main Code.py
@@ -1,11 +1,84 @@
 import vpython as vp
 
+# Notes remove vp. from all labels for code to work
+
+def arrow(**kid):
+    return vp.arrow(pos = kid["pos"], axis = kid["axis"])
+
+def vec(x,y,z):
+    return vp.vec(x,y,z)
+
+def sphere(**kid):
+    return vp.sphere(pos = kid["pos"], radius = kid["radius"])
+
+def radians(number):
+    return vp.radians(number)
+
+# region Coordiante System 
+origin = vec(0, 0, 0)
+axis = sphere(pos = origin, radius = 5)  # Here radius is length of axis
+axis.l = axis.radius  # Lenght of axis arrows
+axis.s = 0.05         # Radius of axis arrows
+axis.f = 'monospace'
+axis.toffset = 0.05
+axis.visible = False
+
+pos_x_axis = arrow(pos = origin, axis = vec(axis.l, 0, 0))
+pos_x_axis.shaftwidth = axis.s
+pos_x_axis.color = vec(1, 0, 0)
+pos_x_axis._round = True
+
+pos_x_axis_label = vp.label(pos = pos_x_axis.pos + pos_x_axis.axis + vec(axis.toffset, 0, 0), 
+         text='+x', height = 16, border = 4, font = axis.f, line = False, opacity = 0, box = False)
+         
+neg_x_axis = arrow(pos = origin, axis = vec(-axis.l, 0, 0))
+neg_x_axis.shaftwidth = axis.s
+neg_x_axis.color = vec(1, 0, 0)
+neg_x_axis._round = True
+
+neg_x_axis_label = vp.label(pos = neg_x_axis.pos + neg_x_axis.axis + vec(-axis.toffset, 0, 0), 
+         text='-x', height = 16, border = 4, font = axis.f, line = False, opacity = 0, box = False)
+         
+pos_y_axis = arrow(pos = origin, axis = vec(0, axis.l, 0))
+pos_y_axis.shaftwidth = axis.s 
+pos_y_axis.color = vec(0, 1, 0)
+pos_y_axis._round=True
+
+pos_y_axis_label = vp.label(pos = pos_y_axis.pos + pos_y_axis.axis + vec(0, axis.toffset, 0), 
+         text='+y', height = 16, border = 4, font = axis.f, line = False, opacity = 0, box = False)
+         
+neg_y_axis = arrow(pos = origin, axis = vec(0, -axis.l, 0))
+neg_y_axis.shaftwidth = axis.s
+neg_y_axis.color = vec(0, 1, 0)
+neg_y_axis._round = True
+
+neg_y_axis_label = vp.label(pos = neg_y_axis.pos + neg_y_axis.axis + vp.vec(0, -axis.toffset, 0), 
+         text='-y', height = 16, border = 4,font = axis.f, line = False, opacity = 0, box = False)
+
+pos_z_axis = arrow(pos = origin, axis = vec(0,0,axis.l))
+pos_z_axis.shaftwidth = axis.s
+pos_z_axis.color = vec(0, 0, 1)
+pos_z_axis._round=True 
+
+
+pos_z_axis_label = vp.label(pos = pos_z_axis.pos + pos_z_axis.axis + vp.vec(0, axis.toffset, 0), 
+         text='+z', height = 16, border = 4, font = axis.f, line = False, opacity = 0, box = False)
+         
+neg_z_axis = arrow(pos = origin, axis = vec(0,0,-axis.l))
+neg_z_axis.shaftwidth = axis.s
+neg_z_axis.color = vp.vec(0, 0, 1)
+neg_z_axis._round = True 
+
+neg_z_axis_label = vp.label(pos = neg_z_axis.pos + neg_z_axis.axis + vp.vec(0, -axis.toffset, 0), 
+         text='-z', height = 16, border = 4,font = axis.f, line = False, opacity = 0, box = False)
+# endregion
+
 coil1 = []
 coil2= []
 R=  2
 N =10
 
-Stage3_L = label(pos = vec(-0, 5, 0), text = 'Stage 3')
+Stage3_L = vp.label(pos = vec(-0, 5, 0), text = 'Stage 3')
 theta_min = radians(0)
 theta_max = radians(360)
 angle_tot = theta_max-theta_min
