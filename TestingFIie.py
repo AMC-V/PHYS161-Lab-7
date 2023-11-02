120c36e58fbf9f156e4af0d145b9e6604f151d4e
John Venom <127551079+Big-Boss-666V@users.noreply.github.com>
2023-11-01 21:40:22 -0700

    Corrected axis and added methods

--- TestingFIie.py
+++ TestingFIie.py
@@ -1,42 +1,69 @@
 import vpython as vp
 
+def arrow(**kid):
+    return vp.arrow(pos = kid["pos"], axis = kid["axis"])
+
+def vec(x,y,z):
+    return vp.vec(x,y,z)
+
+def sphere(**kid):
+    return vp.sphere(pos = kid["pos"], radius = kid["radius"])
 
 # region Coordiante System 
-origin = vp.vec(0, 0, 0)
-axis = vp.sphere(pos = origin, radius = 5)  # Here radius is length of axis
+origin = vec(0, 0, 0)
+axis = sphere(pos = origin, radius = 5)  # Here radius is length of axis
 axis.l = axis.radius  # Lenght of axis arrows
 axis.s = 0.05         # Radius of axis arrows
 axis.f = 'monospace'
 axis.toffset = 0.05
 axis.visible = False
 
-pos_x_axis = vp.arrow(pos = origin, axis = vp.vec(axis.l,0,0), shaftwidth = axis.s, round=True,
-         color = vp.vec(1, 0, 0))
-pos_x_axis_label = vp.label(pos = pos_x_axis.pos + pos_x_axis.axis + vp.vec(axis.toffset, 0, 0), 
+pos_x_axis = arrow(pos = origin, axis = vec(axis.l, 0, 0))
+pos_x_axis.shaftwidth = axis.s
+pos_x_axis.color = vec(1, 0, 0)
+pos_x_axis._round = True
+
+pos_x_axis_label = vp.label(pos = pos_x_axis.pos + pos_x_axis.axis + vec(axis.toffset, 0, 0), 
          text='+x', height = 16, border = 4, font = axis.f, line = False, opacity = 0, box = False)
          
-neg_x_axis = vp.arrow(pos = origin, axis = vp.vector(-axis.l,0,0), shaftwidth = axis.s, round=True,
-         color = vp.vec(1, 0, 0))
-neg_x_axis_label = vp.label(pos = neg_x_axis.pos + neg_x_axis.axis + vp.vec(-axis.toffset, 0, 0), 
+neg_x_axis = arrow(pos = origin, axis = vec(-axis.l, 0, 0))
+neg_x_axis.shaftwidth = axis.s
+neg_x_axis.color = vec(1, 0, 0)
+neg_x_axis._round = True
+
+neg_x_axis_label = vp.label(pos = neg_x_axis.pos + neg_x_axis.axis + vec(-axis.toffset, 0, 0), 
          text='-x', height = 16, border = 4, font = axis.f, line = False, opacity = 0, box = False)
          
-pos_y_axis = vp.arrow(pos = origin, axis = vp.vector(0,axis.l,0), shaftwidth = axis.s, round=True, 
-         color = vp.vec(0, 1, 0))
-pos_y_axis_label = vp.label(pos = pos_y_axis.pos + pos_y_axis.axis + vp.vec(0, axis.toffset, 0), 
+pos_y_axis = arrow(pos = origin, axis = vec(0, axis.l, 0))
+pos_y_axis.shaftwidth = axis.s 
+pos_y_axis.color = vec(0, 1, 0)
+pos_y_axis._round=True
+
+pos_y_axis_label = vp.label(pos = pos_y_axis.pos + pos_y_axis.axis + vec(0, axis.toffset, 0), 
          text='+y', height = 16, border = 4, font = axis.f, line = False, opacity = 0, box = False)
          
-neg_y_axis = vp.arrow(pos = origin, axis = vp.vector(0,-axis.l,0), shaftwidth = axis.s, round=True, 
-         color = vp.vec(0, 1, 0))
+neg_y_axis = arrow(pos = origin, axis = vec(0, -axis.l, 0))
+neg_y_axis.shaftwidth = axis.s
+neg_y_axis.color = vec(0, 1, 0)
+neg_y_axis._round = True
+
 neg_y_axis_label = vp.label(pos = neg_y_axis.pos + neg_y_axis.axis + vp.vec(0, -axis.toffset, 0), 
          text='-y', height = 16, border = 4,font = axis.f, line = False, opacity = 0, box = False)
 
-pos_z_axis = vp.arrow(pos = origin, axis = vp.vector(0,0,axis.l), shaftwidth = axis.s, round=True, 
-         color = vp.vec(0, 0, 1))
+pos_z_axis = arrow(pos = origin, axis = vec(0,0,axis.l))
+pos_z_axis.shaftwidth = axis.s
+pos_z_axis.color = vec(0, 0, 1)
+pos_z_axis._round=True 
+
+
 pos_z_axis_label = vp.label(pos = pos_z_axis.pos + pos_z_axis.axis + vp.vec(0, axis.toffset, 0), 
          text='+z', height = 16, border = 4, font = axis.f, line = False, opacity = 0, box = False)
          
-neg_z_axis = vp.arrow(pos = origin, axis = vp.vector(0,0,-axis.l), shaftwidth = axis.s, round=True, 
-         color = vp.vec(0, 0, 1))
+neg_z_axis = arrow(pos = origin, axis = vec(0,0,-axis.l))
+neg_z_axis.shaftwidth = axis.s
+neg_z_axis.color = vp.vec(0, 0, 1)
+neg_z_axis._round = True 
+
 neg_z_axis_label = vp.label(pos = neg_z_axis.pos + neg_z_axis.axis + vp.vec(0, -axis.toffset, 0), 
          text='-z', height = 16, border = 4,font = axis.f, line = False, opacity = 0, box = False)
 # endregion
